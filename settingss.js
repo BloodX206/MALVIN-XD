954bb6af4da0718c5a806c5784bc6f33bc63cd20
BloodX206 <ayankashkali206@gmail.com>
2025-07-25 12:44:01 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " malvin~ducnBChL#VzpEvwLQGkLRjWb3IhWJ9qWXQUehZxZG8ML9I5sQz7o",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
